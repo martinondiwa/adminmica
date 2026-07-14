c1b5e3dd9c6958d21e05a69957242b60e060b729
martinondiwa <surekondiwa@gmail.com>
2026-07-14 10:21:38 -0400

    including data providers

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,4 +1,6 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, { GraphQLClient,
+    liveProvider as graphqlLiveProvider
+} from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
 import { createClient } from "graphql-ws";
 
@@ -30,3 +32,6 @@ export const wsClient = typeof window !== "undefined"
         }
     })
     :undefined
+
+    export const dataProvider = graphqlDataProvider(client);
+    export const liveProvider = wsClient? graphqlDataProvider(wsClient) : undefined;
