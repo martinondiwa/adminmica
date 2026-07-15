a23d7f8b5928e5e8968cd6223c9af0dce5319249
martinondiwa <surekondiwa@gmail.com>
2026-07-15 06:55:45 -0400

    importing providers

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,7 @@ import {
 } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import {dataProvider, liveProvider} from "./providers"
 import routerProvider, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -51,8 +48,8 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-               dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+               dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerProvider}
                // authProvider={}
