c688cf03b31d23500e0c6a5a9ec5f47a248e7394
martinondiwa <surekondiwa@gmail.com>
2026-07-16 08:36:47 -0400

    raw mutation

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -1,13 +1,33 @@
 import {AuthBindings} from "@refinedev/core";
 
 import {API_URL, dataProvider} from "./data";
+import { Login } from "../pages/login";
 
 //For demo purposes
 export const authCredentials = {
   email: "ondiwamartin@gmail.com",
-  password: "#7014martinSURE",
+  password: "demodemo",
 };
 
 export const authProvider: AuthBindings = {
-    login: async ({email})
+    login: async ({email}) => {
+        try {
+            //call the login mutation
+            const {data} = await dataProvider.custom({
+                url:API_URL,
+                method: "post",
+                headers:{},
+                meta: {
+                    variables: {email},
+                    rawQuery: `
+                    mutation Login($email: String!) {
+                        Login(loginInput: {email:$email}) {
+                            accessToken
+                        }
+                     }
+                    `, 
+                },
+            });
+        }
+    }
 }
