ebb3b694a9b535012cb2d17462824290c7440431
martinondiwa <surekondiwa@gmail.com>
2026-07-16 09:00:06 -0400

    setting logout to true

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -2,6 +2,8 @@ import {AuthBindings} from "@refinedev/core";
 
 import {API_URL, dataProvider} from "./data";
 import { Login } from "../pages/login";
+import { redirect } from "react-router";
+import { message } from "antd";
 
 //For demo purposes
 export const authCredentials = {
@@ -28,6 +30,44 @@ export const authProvider: AuthBindings = {
                     `, 
                 },
             });
+            //save the accessToken in localStorage
+            localStorage.setItem("access_token", data.login.accessToken);
+
+            return {
+                success: true,
+                redirectTo:"/",
+            };
+        } catch (e) {
+                const error = e as Error;
+
+                return {
+                    success: false,
+                    error:{
+                        message: "message" in error ? error.message : "Login failed",
+                        name: "name" in error ? error.name : "Invalid email or password",
+                    },
+                };
+            }
+    },
+    //Simply remove the accessToken from localStorage for the logout
+    logout: async () => {
+        localStorage.removeItem("access_token");
+        
+        return {
+            success: true,
+            redirectTo: "/login",
+        };
+    },
+
+    onError: async (error) => {
+        //This is a check to see if the error is an authentication error
+        //if so, set logout to true
+        if (error.statusCode === "UNAUTHENTICATED") {
+            return {
+                logout: true,
+                ...error,
+            };
         }
-    }
-}
+        return {error};
+    },
+};
