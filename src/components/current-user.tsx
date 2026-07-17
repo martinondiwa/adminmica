dde5a967a67ccd5a4a95cba61fa0c56129ccc245
martinondiwa <surekondiwa@gmail.com>
2026-07-17 13:11:01 -0400

    style for popover

--- src/components/current-user.tsx
+++ src/components/current-user.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 const CureenUser = () => {
   return (
     <>
-      <Popover>
+      <Popover
+      placement='bottomRight'
+      trigger="click"
+      overlayInnerStyle={{padding: 0}}
+      overlayStyle={{zIndex: 999}}
+      >
 
       </Popover>
     </>
