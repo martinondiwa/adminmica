0e87311b261ca646e5861d5e94f2e0a4b8163975
martinondiwa <surekondiwa@gmail.com>
2026-07-28 18:24:17 -0400

    custom avatar

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,22 +1,23 @@
-import { getNameInitials } from "@/utilities";
-import { Avatar as AntdAvatar, AvatarProps } from "antd"
+import { getNameInitials } from '@/utilities';
+import { Avatar as AntdAvatar, AvatarProps } from 'antd'
 
-type props = AvatarProps & {
-    name?: string;
+type Props = AvatarProps & {
+  name?: string;
 }
 
-const CustomAvatar = ({name, style, ...rest}: props) => {
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
     <AntdAvatar
-    alt="Martin Sure"
-    size="small"
-    style={{backgroundColor:"#89d269fd",
+      alt={name}
+      size="small"
+      style={{ 
+        backgroundColor: '#87d068',
         display: 'flex',
         alignItems: 'center',
         border: 'none',
         ...style
-    }}
-   {...rest}
+       }}
+      {...rest}
     >
       {getNameInitials(name || '')}
     </AntdAvatar>
