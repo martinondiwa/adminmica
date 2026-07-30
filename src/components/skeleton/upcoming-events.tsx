3b8e86fae975820bc20a5b965ae63cd341825023
martinondiwa <surekondiwa@gmail.com>
2026-07-30 19:52:31 -0400

    upcoming events

--- src/components/skeleton/upcoming-events.tsx
+++ src/components/skeleton/upcoming-events.tsx
@@ -1,21 +1,31 @@
-import { Skeleton } from "antd";
+import { Badge, List, Skeleton } from "antd";
 
-// create a skeleton for the accordion header
-const AccordionHeaderSkeleton = () => {
+const UpcomingEventsSkeleton = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: "8px",
-        padding: "12px 24px",
-        borderBottom: "1px solid #d9d9d9",
-      }}
-    >
-      <Skeleton.Avatar size="small" shape="square" />
-      <Skeleton.Input size="small" block style={{ height: "22px" }} />
-    </div>
+    <List.Item>
+      <List.Item.Meta
+        avatar={<Badge color="transparent" />}
+        title={
+          <Skeleton.Button
+            active
+            style={{
+              height: "14px",
+            }}
+          />
+        }
+        description={
+          <Skeleton.Button
+            active
+            style={{
+              width: "300px",
+              marginTop: "8px",
+              height: "16px",
+            }}
+          />
+        }
+      />
+    </List.Item>
   );
 };
 
-export default AccordionHeaderSkeleton;
+export default UpcomingEventsSkeleton;
